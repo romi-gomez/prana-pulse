cb7907c7f2cd12c59835d5ae325a3759e6012f1a
Romi Gomez <contact@romigomez.com.ar>
2024-07-14 19:12:17 -0300

    chore(setup): configure dotenv for environment variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'  // Ensure the correct import
+
+import dotenv from 'dotenv'
+
+// Load environment variables from .env file
+dotenv.config()
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   plugins: [{ src: '~/plugins/firebase.js', mode: 'client' }],
   head: {
     link: [
-      { rel: 'stylesheet', href: 'https://use.typekit.net/wwt7sct.css' } 
+      { rel: 'stylesheet', href: 'https://use.typekit.net/wwt7sct.css' }
     ]
   },
   css: [
@@ -15,5 +21,17 @@ export default defineNuxtConfig({
     postcss: {
       postcssOptions: require('./postcss.config.js')
     }
+  },
+  runtimeConfig: {
+    public: {
+      googleApiKey: process.env.GOOGLE_API_KEY,
+      googleAuthDomain: process.env.GOOGLE_AUTH_DOMAIN,
+      googleProjectId: process.env.GOOGLE_PROJECT_ID,
+      googleStorageBucket: process.env.GOOGLE_STORAGE_BUCKET,
+      googleMessagingSenderId: process.env.GOOGLE_MESSAGING_SENDER_ID,
+      googleAppId: process.env.GOOGLE_APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+    },
+    apiSecret: process.env.API_SECRET,
   }
 })
